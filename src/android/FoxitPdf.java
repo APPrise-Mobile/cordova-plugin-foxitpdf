3c2073753fc6e36a781af7e942844d7ccd5b4470
davearata <dave.arata@dacatech.com>
2017-06-28 22:14:25 -0400

    android working now

--- src/android/FoxitPdf.java
+++ src/android/FoxitPdf.java
@@ -4,11 +4,17 @@ import android.content.Context;
 import android.graphics.Color;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.app.Activity;
 
 import com.foxit.sdk.PDFViewCtrl;
 import com.foxit.sdk.common.Library;
 import com.foxit.sdk.common.PDFException;
 import com.foxit.uiextensions.UIExtensionsManager;
+import com.foxit.uiextensions.pdfreader.impl.PDFReader;
+
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.Charset;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -21,7 +27,6 @@ import org.json.JSONException;
  */
 public class FoxitPdf extends CordovaPlugin {
     private static int errCode = PDFException.e_errSuccess;
-    private PDFViewCtrl pdfViewCtrl = null;
     static {
         System.loadLibrary("rdk");
     }
@@ -33,8 +38,8 @@ public class FoxitPdf extends CordovaPlugin {
             final String key = args.getString(1);
             this.init(sn, key, callbackContext);
             return true;
-        } else if (action.equals("Preview")){
-            final String filePath = args.getString(0);
+        } else if (action.equals("openPdf")){
+            final String filePath = args.getString(0).replace("file://", "");
             this.openDoc(filePath, callbackContext);
             return true;
         }
@@ -65,30 +70,56 @@ public class FoxitPdf extends CordovaPlugin {
         }
 
         final Context context = this.cordova.getActivity();
+        final Activity activity =  this.cordova.getActivity();
         this.cordova.getActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                final RelativeLayout relativeLayout = new RelativeLayout(context);
-                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-                        RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+              final RelativeLayout relativeLayout = new RelativeLayout(context);
+              RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                      RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
 
-                pdfViewCtrl = new PDFViewCtrl(context);
+              final PDFViewCtrl pdfViewCtrl = new PDFViewCtrl(context);
 
-                relativeLayout.addView(pdfViewCtrl, params);
-                relativeLayout.setWillNotDraw(false);
-                relativeLayout.setBackgroundColor(Color.argb(0xff, 0xe1, 0xe1, 0xe1));
-                relativeLayout.setDrawingCacheEnabled(true);
-                setContentView(relativeLayout);
+              relativeLayout.addView(pdfViewCtrl, params);
+              relativeLayout.setWillNotDraw(false);
+              relativeLayout.setBackgroundColor(Color.argb(0xff, 0xe1, 0xe1, 0xe1));
+              relativeLayout.setDrawingCacheEnabled(true);
+              setContentView(relativeLayout);
 
-                UIExtensionsManager uiExtensionsManager = new UIExtensionsManager(context,
-                        relativeLayout, pdfViewCtrl);
+              final String UIExtensionsConfig = "{\n" +
+                  "    \"defaultReader\": true,\n" +
+                  "    \"modules\": {\n" +
+                  "        \"readingbookmark\": true,\n" +
+                  "        \"outline\": true,\n" +
+                  "        \"annotations\": true,\n" +
+                  "        \"thumbnail\" : false,\n" +
+                  "        \"attachment\": true,\n" +
+                  "        \"signature\": false,\n" +
+                  "        \"search\": true,\n" +
+                  "        \"pageNavigation\": true,\n" +
+                  "        \"form\": true,\n" +
+                  "        \"selection\": true,\n" +
+                  "        \"encryption\" : false\n" +
+                  "    }\n" +
+                  "}\n";
 
-                pdfViewCtrl.setUIExtensionsManager(uiExtensionsManager);
+              InputStream stream = new ByteArrayInputStream(UIExtensionsConfig.getBytes(Charset.forName("UTF-8")));
+              UIExtensionsManager.Config config = new UIExtensionsManager.Config(stream);
 
-                pdfViewCtrl.openDoc(path, null);
-                callbackContext.success("Open document success.");
+              UIExtensionsManager uiextensionsManager = new UIExtensionsManager(context, relativeLayout, pdfViewCtrl,config);
+              uiextensionsManager.setAttachedActivity(activity);
+
+              pdfViewCtrl.setUIExtensionsManager(uiextensionsManager);
+
+              PDFReader mPDFReader= (PDFReader) uiextensionsManager.getPDFReader();
+              mPDFReader.onCreate(activity, pdfViewCtrl, null);
+              mPDFReader.openDocument(path, null);
+              setContentView(mPDFReader.getContentView());
+              mPDFReader.onStart(activity);
             }
         });
+
+//        callbackContext.success("Open document success.");
     }
 
     private void setContentView(View view) {
